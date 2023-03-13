a0beac2124a2a1bbbb626d0369c49d46bc869f53
lizramos611 <111591265+lizramos611@users.noreply.github.com>
2023-03-13 19:44:11 -0400

    updating buttons

--- develop/index.js
+++ develop/index.js
@@ -1,54 +1,54 @@
-const currentDay = document.querySelector('$currentDay');
+let currentDay = document.querySelector('#currentDay');
 //day for current day display
 currentDay.innerHTML = moment().format('dddd MMM Do YYYY');
 
 //buttons to save
-const saveButton1 = document.querySelector('#save-button-1');
-const saveButton2 = document.querySelector('#save-button-2');
-const saveButton3 = document.querySelector('#save-button-3');
-const saveButton4 = document.querySelector('#save-button-4');
-const saveButton5 = document.querySelector('#save-button-5');
-const saveButton6 = document.querySelector('#save-button-6');
-const saveButton7 = document.querySelector('#save-button-7');
-const saveButton8 = document.querySelector('#save-button-8');
-const saveButton9 = document.querySelector('#save-button-9');
+let saveButton1 = document.querySelector('#save-button-1');
+let saveButton2 = document.querySelector('#save-button-2');
+let saveButton3 = document.querySelector('#save-button-3');
+let = document.querySelector('#save-button-4');
+let saveButton5 = document.querySelector('#save-button-5');
+let saveButton6 = document.querySelector('#save-button-6');
+let saveButton7 = document.querySelector('#save-button-7');
+let saveButton8 = document.querySelector('#save-button-8');
+let saveButton9 = document.querySelector('#save-button-9');
 //what the user is typing in
-const userInput1 = document.querySelector('#user-form-1');
-const userInput2 = document.querySelector('#user-form-2');
-const userInput3 = document.querySelector('#user-form-3');
-const userInput4 = document.querySelector('#user-form-4');
-const userInput5 = document.querySelector('#user-form-5');
-const userInput6 = document.querySelector('#user-form-6');
-const userInput7 = document.querySelector('#user-form-7');
-const userInput8 = document.querySelector('#user-form-8');
-const userInput9 = document.querySelector('#user-form-9');
+let userInput1 = document.querySelector('#user-form-1');
+let userInput2 = document.querySelector('#user-form-2');
+let userInput3 = document.querySelector('#user-form-3');
+let userInput4 = document.querySelector('#user-form-4');
+let userInput5 = document.querySelector('#user-form-5');
+let userInput6 = document.querySelector('#user-form-6');
+let userInput7 = document.querySelector('#user-form-7');
+let userInput8 = document.querySelector('#user-form-8');
+let userInput9 = document.querySelector('#user-form-9');
 
 let userStoredData1 = localStorage.getItem('userInput1');
 userInput1.textContent = userStoredData1;
 
 let userStoredData2 = localStorage.getItem('userInput2');
-userInput2.textContent = userStoredData1;
+userInput2.textContent = userStoredData2;
 
 let userStoredData3 = localStorage.getItem('userInput3');
-userInput3.textContent = userStoredData1;
+userInput3.textContent = userStoredData3;
 
 let userStoredData4 = localStorage.getItem('userInput4');
-userInput4.textContent = userStoredData1;
+userInput4.textContent = userStoredData4;
 
 let userStoredData5 = localStorage.getItem('userInput5');
-userInput5.textContent = userStoredData1;
+userInput5.textContent = userStoredData5;
 
 let userStoredData6 = localStorage.getItem('userInput6');
-userInput6.textContent = userStoredData1;
+userInput6.textContent = userStoredData6;
 
 let userStoredData7 = localStorage.getItem('userInput7');
-userInput7.textContent = userStoredData1;
+userInput7.textContent = userStoredData7;
 
 let userStoredData8 = localStorage.getItem('userInput8');
-userInput8.textContent = userStoredData1;
+userInput8.textContent = userStoredData8;
 
 let userStoredData9 = localStorage.getItem('userInput9');
-userInput9.textContent = userStoredData1;
+userInput9.textContent = userStoredData9;
 
 //event listens to save items
 
@@ -64,7 +64,7 @@ saveButton3.addEventListener('click', function () {
     let userInput3 = document.querySelector('#user-form-3').value;
     localStorage.setItem('userInput3', userInput3);
 })
-saveButton4.addEventListener('click', function () {
+let.addEventListener('click', function () {
     let userInput4 = document.querySelector('#user-form-4').value;
     localStorage.setItem('userInput4', userInput4);
 })
